342f592eedbcd34f3f84ba102c35669ac04823f2
raps1304 <125717427+raps1304@users.noreply.github.com>
2023-02-18 11:11:57 +0700

    Add files via upload

---
+++ ALOIREN.go
@@ -0,0 +1,45 @@
+<html><head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title>repl.it</title>
+    <link href="style.css" rel="stylesheet" type="text/css">
+    <script>
+      alert("Aloo irenne:P")
+      alert("welcome to part 1")
+      alert("HEHEHEHE")
+      alert("oiya lupaa")
+      alert("How r u??")
+	  alert("Wyd rn???")
+	  alert("hope u doin well")
+      alert("semangaat main piano menghitung harinyaa :p")
+	  alert("semangat 3x")
+	  alert("HAHAHAHAHA")
+	  alert("Wait, wait")
+      alert("ren, you have something on your face ")
+      alert("wait,its just the most beautiful smile i've ever seen :p")
+      alert("HAHAHAHA GOT YA")
+      alert("bercanda bercanda")
+      alert("aku emang suka gajelas")
+      alert("tapi kalo suka kamu itu udah jelas😎HAHAHAHAHA")
+      alert("maaf dari reels")
+      alert("ikan hiu ngedek ngedek")
+      alert("aylafyu dek🥵🥵💓💘💖💕🌈❤️💖💓💘💕")
+      alert("HAHAHAHAHAHA DARI TIKTOK JG MAAF")
+	  alert("BECANDA DEMI ALLAH BECANDA")
+	  alert("serius juga gpp si :P ")
+	  alert("becanda lagi....")
+      alert("mmmm")
+      alert("u know u can tell me about ur day or ur problems to me owtay?")
+      alert("maybe if u get disturbed or uncomfy wth this shit")
+	  alert("bakal jd yg pertama and terakhir hehe")
+      alert("dont forgett to smilee")
+      alert("oiya maaf ya fi cuma iseng gabut")
+      alert("Good night🤏🥵")
+      alert("have a nice dayyy☺️😱😎👍🤏☺️❤️👍🌝☺️❤️☺️❤️🌝😎👍😎❤️😙❤️🙃🤏🌝❤️💔❤️🤪")
+	  alert("bubay")
+    </script>
+  </head>
+  <body>
+    <script src="script.js"></script>
+  
+HAVEANICEDAYHAVEANICEDAYHAVEANICEDAYHAVEANICEDAYHAVEANICEDAYHAVEANICEDAYHAVEANICEDAYHAVEANICEDAYHAVEANICEDAYHAVEANICEDAYHAVEANICEDAYHAVEANICEDAYHAVEANICEDAYHAVEANICEDAYHAVEANICEDAYHAVEANICEDAYHAVEANICEDAYHAVEANICEDAYHAVEANICEDAYHAVEANICEDAYHAVEANICEDAYHAVEANICEDAYHAVEANICEDAYHAVEANICEDAYHAVEANICEDAY</body></html>
